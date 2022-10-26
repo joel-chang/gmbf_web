84b5334143560561c3b70ad58d6bc583d0b37330
Joel Chang <jcchangg3@gmail.com>
2022-10-27 01:34:01 +0800

    Revamp website structure
    Should probably look into version bumping and all that.
    
    Major changes:
    - Website structure.
    	Static .html pages are now components.
    	Finally "routered" the site, will make things easier.
    	UI improvements.
    
    Minor changes:
    - Some typos in "recommended" and "learn more" sections.
    
    Center website in big screeens
    
    style typo
    
    remove creds

--- src/creds.js
+++ src/creds.js
@@ -1,10 +1,10 @@
 export const CREDS = {
-    apiKey: "AIzaSyCHQPJu-sqW0VTDvu4mgG69hopsCR-79-o",
-    authDomain: "reacttest-a9592.firebaseapp.com",
-    databaseURL: "https://reacttest-a9592-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "reacttest-a9592",
-    storageBucket: "reacttest-a9592.appspot.com",
-    messagingSenderId: "462483936284",
-    appId: "1:462483936284:web:a65c828065985e9737fbd9",
-    measurementId: "G-XEEE17Y5C0"
+    apiKey: "AI o",
+    authDomain: "r m",
+    databaseURL: " ",
+    projectId: " -a9592",
+    storageBucket: "re",
+    messagingSenderId: "",
+    appId: "1",
+    measurementId: "G-"
     };
